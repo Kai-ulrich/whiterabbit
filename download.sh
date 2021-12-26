95f42d2fef086c72d566b9e4ee25219ac5e62e5d
Kai-ulrich <46796948+Kai-ulrich@users.noreply.github.com>
2021-12-26 12:20:35 -0800

    Add files via upload

--- download.sh
+++ download.sh
@@ -21,7 +21,7 @@ echo "Enter a number 1-9"
 read n
 
 if [ $n -eq 1 ];
-then 
+then
 echo "Downloading Byob by Malwaredllc"
 sudo apt-get install git
 sudo git clone https://github.com/malwaredllc/byob
@@ -36,15 +36,27 @@ sudo apt-get update
 elif [ $n -eq 2 ];
 then
 echo "Downloading Proxmark3 RDV4"
+sudo apt-get install git ca-certificates build-essential pkg-config libreadline-dev gcc-arm-none-eabi libnewlib-dev qtbase5-dev
+git clone https://github.com/RfidResearchGroup/proxmark3.git
+sudo apt remove modemmanager
+sudo systemctl stop ModemManager
+sudo systemctl disable ModemManager
+sudo dmesg | grep -i usb
+cd proxmark3
+git pull
+sudo apt-get install --no-install-recommends git ca-certificates build-essential pkg-config \ libreadline-dev gcc-arm-none-eabi libnewlib-dev qtbase5-dev libbz2-dev libbluetooth-dev libpython3-dev
+make clean && make all
+sudo make install
+sudo apt-get update
 
 elif [ $n -eq 3 ];
-then 
+then
 echo "Downloading Beef-XSS"
 sudo apt-get install beef-xss
 sudo apt-get update
 
 elif [ $n -eq 4 ];
-then 
+then
 echo "Downloading Sherlock"
 git clone https://github.com/sherlock-project/sherlock.git
 cd sherlock
@@ -52,27 +64,27 @@ python3 -m pip install -r requirement.txt
 sudo apt-get update
 
 elif [ $n -eq 5 ];
-then 
+then
 echo "Downloading PhoneInfoga"
 echo "Working on configuration"
 
 elif [ $n -eq 6 ];
-then 
+then
 echo "Downloading ProtonVPN"
 echo "Working on configuration"
 
 elif [ $n -eq 7 ];
-then 
+then
 echo "Update"
 sudo apt-get update
 
 elif [ $n -eq 8 ];
-then 
+then
 echo "Upgrade"
 sudo apt-get upgrade -y
 
 elif [ $n -eq 9 ];
-then 
+then
 echo "Exiting"
 exit
 
